04f25754a302f9617fe6304504ab7f6ce9e5a0f8
Jem <0x0xjem@gmail.com>
2022-03-10 16:54:42 +0400

    Debug

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const core = require('@actions/core');
 
 try {
+  console.log("inputs " + core.getInput('inputsToCheck'));
   const inputsToCheck = core.getInput('inputsToCheck').split(',');
   const failOnMissing = core.getInput('failOnMissing') == 'true';
 
   let missingInputs = [];
 
-  if(failOnMissing || (inputsToCheck.length === 0 || (inputsToCheck.length === 1 && inputsToCheck[0] === '') )) {
+  if (failOnMissing || (inputsToCheck.length === 0 || (inputsToCheck.length === 1 && inputsToCheck[0] === ''))) {
     throw new Error(`'inputsToCheck' is missing, please include it in your workflow section 'with' as inputsToCheck: 'input1,input2,...'`)
   }
 
   core.startGroup(`Checking ${inputsToCheck.length} inputs:`)
   inputsToCheck.forEach((input) => {
     const val = process.env[input] || core.getInput(input);
-    if(!val || val === '') {
+    if (!val || val === '') {
       missingInputs.push(input);
       core.warning(` - ❌ '${input}' is empty`)
     } else {
